e294f3906f66c036ef136e871d89c0157f0a60ba
user <krysanmihasan@gmail.com>
2022-07-03 12:21:34 +0300

    fourth commit - lesson 13

--- app/src/main/java/com/appbymikekrysan/ka_lesson10/MainActivity.kt
+++ app/src/main/java/com/appbymikekrysan/ka_lesson10/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.appbymikekrysan.ka_lesson10
 
+import android.os.Build.ID
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.appbymikekrysan.ka_lesson10.constance.Constance
 import com.appbymikekrysan.ka_lesson10.databinding.ActivityMainBinding
@@ -16,40 +18,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
         bindingClass.btResult.setOnClickListener {
+//            Log.d("MyLog", "ID dulia = ${R.drawable.dulia}")
             val resultValue = bindingClass.edValue.text.toString()
             when(resultValue) {
                 Constance.ENGINEER->  {
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.ENGINEER_SALARY}"
-                    (bindingClass.tvResult.text) = if(bindingClass.edPassword.text.toString() == Constance.ENGINEER_PASSWORD){
-                        tempText
+                     if(bindingClass.edPassword.text.toString() == Constance.ENGINEER_PASSWORD){
+                         bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.andrey)
                     } else {
-                        "Неверный пароль"
+                         bindingClass.tvResult.text = "Неверный пароль"
+//                         bindingClass.imPhoto.setImageResource("2131165286".toInt())
+                         bindingClass.imPhoto.setImageResource(R.drawable.dulia)
+//                         val idDulia = 2131165286
+//                         bindingClass.imPhoto.setImageResource(idDulia)
                     }
                 }
                 Constance.CLEANER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.CLEANER_SALARY}"
-                    (bindingClass.tvResult.text) = if(bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD){
-                        tempText
+                    if(bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.sergey)
                     } else {
-                        "Неверный пароль"
+                        (bindingClass.tvResult.text) = "Неверный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dulia)
                     }
                 }
 
                 Constance.DIRECTOR -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Получите ваши ${Constance.DIRECTOR_SALARY}"
-                    (bindingClass.tvResult.text) = if(bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD){
-                        tempText
+                    if(bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.egor)
                     } else {
-                        "Неверный пароль"
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dulia)
                     }
                 }
 
                     else -> {
                         bindingClass.tvResult.visibility = View.VISIBLE
                         bindingClass.tvResult.text = "Нет такого работника"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dulia)
+
                     }
             }
 
