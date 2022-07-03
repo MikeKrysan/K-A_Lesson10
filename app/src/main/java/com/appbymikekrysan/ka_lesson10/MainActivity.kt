e3c2415c7355798d13b07393982ab63a1de833c9
user <krysanmihasan@gmail.com>
2022-07-03 09:02:08 +0300

    second commit - lesson 11

--- app/src/main/java/com/appbymikekrysan/ka_lesson10/MainActivity.kt
+++ app/src/main/java/com/appbymikekrysan/ka_lesson10/MainActivity.kt
@@ -3,96 +3,38 @@ package com.appbymikekrysan.ka_lesson10
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.appbymikekrysan.ka_lesson10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
-    val maxPerson = 90
-    val currentPerson = 93
-    val text = "опасность"
-    val text2 = "споко"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-//        bindingClass.b1.setOnClickListener{
-//            bindingClass.tvResult.text = if(maxPerson > currentPerson)
-//                "Все в порядке"
-//            else
-//                "Превышено максимальное количество посетителей "
-//        }
-
-//        bindingClass.b1.setOnClickListener{
-//            if(maxPerson > currentPerson) {
-//                bindingClass.tvResult.text = "Все в порядке"
-//                bindingClass.tvResult.setBackgroundColor(Color.GREEN)
-//            }
-//                else {
-//                bindingClass.tvResult.text = "Превышено максимальное количество посетителей "
-//                bindingClass.tvResult.setBackgroundColor(Color.RED)
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            when(currentPerson) {
-//                in 0..maxPerson -> bindingClass.tvResult.text = "Все в порядке"
-//                else -> bindingClass.tvResult.text = "Предел превышен"
-//
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            bindingClass.tvResult.text = when(currentPerson) {
-//                in 0..maxPerson -> "Все в порядке"
-//                else -> "Предел превышен"
-//
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            bindingClass.tvResult.text = when(currentPerson) {
-//                !in 0..maxPerson -> "Предел превышен"
-//                else -> "Все в порядке"
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            bindingClass.tvResult.text = when(currentPerson) {
-//                90,91,92 -> "Предел превышен"
-//                else -> "Все в порядке"
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            bindingClass.tvResult.text = when(maxPerson > currentPerson) {
-//                false -> "Предел превышен"
-//                true -> "Все в порядке"
-//            }
-//        }
-
-//        bindingClass.b1.setOnClickListener {
-//            bindingClass.tvResult.text = when(text) {
-//                text2 -> "Предел превышен"
-//                text -> "Все в порядке"
-//                    else -> "Не понятно"
-//            }
-//        }
-
-        bindingClass.b1.setOnClickListener {
-            when(text) {
-                text2 -> bindingClass.tvResult.text = "Предел превышен"
-                text -> bindingClass.tvResult.text = "Все в порядке"
+        bindingClass.btResult.setOnClickListener {
+            val resultValue = bindingClass.edValue.text.toString().toInt()
+            when(resultValue) {
+                in 0..1000 ->  {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Вы начинающий блогер"
+                }
+                in 1000..100000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Вы средний блогер"
+                }
+                    else -> {
+                        bindingClass.tvResult.visibility = View.VISIBLE
+                        bindingClass.tvResult.text = "Вы суперзвезда!"
+                    }
             }
-        }
-
-        bindingClass.b2.setOnClickListener{
 
         }
 
-        bindingClass.b3.setOnClickListener{
 
-        }
     }
 }
